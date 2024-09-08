5af0c7eda8184fbe80764848367947d85a41eb59
marcosanalyst <analyst.marcos@gmail.com>
2024-09-08 13:00:17 -0300

    Variáveis com let, casesensentive e camelcase

--- README.md
+++ README.md
@@ -4,21 +4,22 @@
 Aula 1
 - Criação de arquivos .js
 - Uso do console.log
-
 Aula 7
 - String 
 - Number 
-
 Aula 8
 - Desafio de uso do console.log
-
 Aula 11
 - Comentários
-
 Aula 12
 - TAG < script > em arquivo HTML
+Aula 13
+- Variáveis com let, camelcase e casesensitive
+
+
 ## Repositório original para consulta
 https://github.com/luizomf/curso-js
 
 ## Professor
+Luiz Otávio Miranda
 Tales Calogi Malaquias
---
+++ aula6/index.js
@@ -0,0 +1,28 @@
+// abstraindo nome João
+//@ts-ignore
+let nome = 'João'; // String
+
+// posso criar uma variável e não iniciá-la, ela está com o valor indefinido (undefined)
+let nascimento;
+
+// inicializando a variável
+nasciment = 1988;
+
+console.log( nome, 'nasceu em 1988.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log( nome, 'casou com Maria em 2012.');
+console.log('O filho de', nome,'se chama Eduardo.');
+
+// variáveis precisam ter nomes significativo
+// Não podem iniciar com número
+// Não podem conter traços " - " ou espaço
+
+// Exemplo camelCase
+
+let nomeCliente = "Mariane";
+let sobreNomeCliente = "Souza";
+
+// Exemplo caseSensitive
+
+let modeloCarro = 'Civic'; 
+let modelocarro = 'Sentra';
