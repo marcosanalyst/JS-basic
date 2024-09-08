41ac80f83d63e488ef0c4d15f3f4059bda747d82
marcosanalyst <analyst.marcos@gmail.com>
2024-09-07 21:15:21 -0300

    Aula7 console.log + String + Number

---
+++ aula3/index.js
@@ -0,0 +1,20 @@
+// método
+// ;  é opcional no JS
+
+
+// String
+// valor literal em aspas simples,duplas ou crase
+console.log('Marcos Silva 1'); 
+console.log("Marcos Silva 2");
+console.log(`Marcos Silva 3`);
+ // uso de ambos
+console.log("Marcos Silva apelido 'Marcão'");
+console.log('Marcos Silva apelido "Marcão"') 
+
+// Number
+
+console.log(123456); 
+console.log(123456, 155577.88);
+console.log(10,20,30,40,50);
+
+
